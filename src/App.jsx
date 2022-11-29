ce1d903458ab074923eab98e2881b6f18a05dd64
Renan Corrêa Pedroso <renan.rcp16@gmail.com>
2022-11-29 17:30:40 -0300

    adicionando responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
 		<Header/>
-		<div className="max-w-[70rem] my-8 mx-auto px-4 grid grid-cols-[256px_1fr] auto-cols-max gap-8 items-start">
+		<div className="max-w-[70rem] my-8 mx-auto px-4 grid md:grid-cols-[256px_1fr] auto-cols-max gap-8 items-start wrapper">
 			<Sidebar/>
 			
 			<main>
