ae214ceb894bff51272d4b2c6ffffa52de72f1ce
Renan Corrêa Pedroso <renan.rcp16@gmail.com>
2022-11-29 16:17:16 -0300

    formulario de comentarios

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -24,6 +24,14 @@ export function Post(props) {
 					<a className="font-bold text-green-600 hover:text-green-500 mr-1" href="">#rocketseat</a>
 				</p>
 			</div>
+
+			<form className="w-full mt-6 pt-6 border-t-2 border-gray-600 group">
+				<strong className="leading-[1.6] text-gray-100">Deixe seu feedback</strong>
+				<textarea className="w-full bg-gray-900 border-0 resize-none h-24 rounded-xl text-gray-100 mt-1 leading-[1.4] p-4" placeholder="Deixe seu comentário" />
+				<footer className="invisible max-h-0 group-focus-within:visible group-focus-within:max-h-[none]">
+					<button className="px-6 py-4 mt-4 rounded-xl border-0 bg-green-600 text-white font-bold cursor-pointer hover:bg-green-400 transition" type="submit">Publicar</button>
+				</footer>
+			</form>
 		</article>
 	)
 }
